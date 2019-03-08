c7e53825d776345ce38b1394bf86e2834fc46a20
adminph-de <36226934+adminph-de@users.noreply.github.com>
2019-03-08 11:14:49 +0100

    v1.0

---
+++ ps-scripts/MountStorageAccount/Mount-StorageAccountShare.ps1
@@ -0,0 +1,25 @@
+﻿function Mount-StorageAccountShare {
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]$File
+)
+
+    $JSON = Get-Content $File | Out-String | ConvertFrom-Json
+
+    $AcctKey = ConvertTo-SecureString -String $JSON.parameters.StorageAccountKey.value -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $JSON.parameters.StorageAccountUser.value, $acctKey
+    if ((Test-Path -Path ($JSON.parameters.StorageAccountShareDirve.value + ":\") )) {
+          Remove-PSDrive -Name $JSON.parameters.StorageAccountShareDirve.value
+    }
+    else {
+       Write-Host
+       Write-Host "Mounting in progress....." -ForegroundColor Red
+       Write-Host 
+       New-PSDrive `
+        -Name $JSON.parameters.StorageAccountShareDirve.value `
+        -PSProvider FileSystem -Root ("\\" + $JSON.parameters.StorageAccountName.value + ".file.core.windows.net\" + $JSON.parameters.StorageAccountShare.value) `
+        -Credential $credential -Persist   
+    }
+
+}
---
+++ ps-scripts/MountStorageAccount/README.md
@@ -0,0 +1,3 @@
+# Azure-ResourceGroupCleanup
+Small Script that mounts a File Share of a Storage Account<br>
+<u><b>FUNNCTION SAMPLE:</u> Mount-StorageAccount -File Mount-StorageAccountShare.parameter.json</b>
